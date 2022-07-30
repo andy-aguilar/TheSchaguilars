408905e5c54f9b5b6dd069eb6678e9aff7151a80
Andy Aguilar <aaguil3@gmail.com>
2022-07-30 08:12:59 -0400

    Remove default create-react-app boilerplate

--- src/spec/App.test.tsx
+++ src/spec/App.test.tsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
-import { App } from './App';
+import { App } from '../App';
 
-test('renders the wedding website h1 link', () => {
+test('renders the wedding website h1', () => {
   render(<App />);
   const heading = screen.getByText(/Kristin and Andy/i);
   expect(heading).toBeInTheDocument();
