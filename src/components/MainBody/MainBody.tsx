47e6c9dc50f86c8ec36c825c98d1d6cc430a5e23
Andy Aguilar <aaguil3@gmail.com>
2022-07-30 09:56:15 -0400

    fix husky test

--- src/components/MainBody/MainBody.tsx
+++ src/components/MainBody/MainBody.tsx
@@ -1,12 +1,7 @@
 import React from "react";
 
 const MainBody = () => {
-  return <div>
-
-
-
-      
-  </div>;
+  return <div></div>;
 };
 
 export default MainBody;
--- src/spec/App.test.tsx
+++ src/spec/App.test.tsx
@@ -1,9 +1,14 @@
 import React from "react";
 import { render, screen } from "@testing-library/react";
 import { App } from "../App";
+import { BrowserRouter } from "react-router-dom";
 
 test("renders the wedding website h1", () => {
-  render(<App />);
+  render(
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  );
   const heading = screen.getByText(/Kristin and Andy/i);
   expect(heading).toBeInTheDocument();
 });
