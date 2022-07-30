0081e56927ac3327bb26f7bf64cc4d3ad8a25b81
Andy Aguilar <aaguil3@gmail.com>
2022-07-30 09:07:12 -0400

    Add test routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,16 @@
 import React, { FunctionComponent } from 'react';
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
+import MainBody from './components/MainBody/MainBody';
 
  export const App: FunctionComponent = () => {
   return (
     <div className="App">
       <h1>Kristin and Andy's Wedding Website</h1>
+      <Routes>
+        <Route path='/' element={<MainBody />} />
+        <Route path='testroute' element={<h1>It worked</h1>} />
+      </Routes>
     </div>
   );
 }
