466d9df4719fedad6abaa61123923f017bac8b5d
Andy Aguilar <aaguil3@gmail.com>
2022-07-30 08:06:02 -0400

    Update App.tsx and remove unused code

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,9 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
-import App from './App';
+import { App } from './App';
 
-test('renders learn react link', () => {
+test('renders the wedding website h1 link', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const heading = screen.getByText(/Kristin and Andy/i);
+  expect(heading).toBeInTheDocument();
 });
