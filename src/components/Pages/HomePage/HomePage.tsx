7a03f8cfbdeeac4a1ded8a8663af236a905b0358
Andy Aguilar <aaguil3@gmail.com>
2022-07-31 08:09:08 -0400

    Add responsive logo for main screen

--- src/components/Pages/HomePage/HomePage.tsx
+++ src/components/Pages/HomePage/HomePage.tsx
@@ -1,11 +1,23 @@
 import React, { FunctionComponent } from "react";
 import "./HomePage.css";
+import useMediaQuery from "@mui/material/useMediaQuery";
 
 export interface Props {
   setIsMenuOpen: (isMenuOpen: boolean) => void;
 }
 
+const narrowLogo: string =
+  "https://the-schaguilars.s3.us-east-2.amazonaws.com/kristinandandynarrowlogo.png";
+const wideLogo: string =
+  "https://the-schaguilars.s3.us-east-2.amazonaws.com/kristin-and-andy.png";
+
 export const HomePage: FunctionComponent<Props> = ({ setIsMenuOpen }) => {
+  const narrow = useMediaQuery("(max-width:768px)");
+
+  function generateImageUrl(): string {
+    return narrow ? narrowLogo : wideLogo;
+  }
+
   return (
     <div
       id={"home-container"}
@@ -16,7 +28,7 @@ export const HomePage: FunctionComponent<Props> = ({ setIsMenuOpen }) => {
       <div id={"home-content"}>
         <img
           onClick={() => setIsMenuOpen(true)}
-          src="https://the-schaguilars.s3.us-east-2.amazonaws.com/kristin-and-andy.png"
+          src={generateImageUrl()}
           alt="kristin and andy, antigua, guatemala, 03, 11, 23"
         ></img>
       </div>
