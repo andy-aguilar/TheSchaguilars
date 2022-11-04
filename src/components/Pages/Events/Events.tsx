8e5cd3a878b59f2d981447607c415775d0a5b8b0
Andy Aguilar <aaguil3@gmail.com>
2022-11-03 22:13:48 -0400

    Add volcano hike

--- src/components/Pages/Events/Events.tsx
+++ src/components/Pages/Events/Events.tsx
@@ -47,6 +47,16 @@ const events: EventInterface[] = [
     directionsUrl:
       "https://www.google.com/maps/place/Pensativo+House+Hotel/@14.5517165,-90.735557,17z/data=!3m1!4b1!4m8!3m7!1s0x85890e6d3d349ab5:0xaabaf4efdd3252f7!5m2!4m1!1i2!8m2!3d14.5517165!4d-90.7333683",
   },
+  {
+    name: "volcano hike",
+    date: "03 09 23",
+    location: "Details coming soon",
+    time: "",
+    attire: "",
+    imgUrl:
+      "https://the-schaguilars.s3.us-east-2.amazonaws.com/antigua-volcano.jpeg",
+    directionsUrl: "",
+  },
 ];
 
 export const Events: FunctionComponent = () => {
